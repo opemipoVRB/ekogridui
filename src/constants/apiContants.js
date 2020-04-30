cbf41a731c9c8b6d679311293989e55285b422b2
opemipoVRB <opemipodurodola@gmail.com>
2020-04-30 17:32:04 +0100

    initial commit

--- src/constants/apiContants.js
+++ src/constants/apiContants.js
@@ -1,1 +1,9 @@
-export const API_BASE_URL = 'http://127.0.0.1:8000/';
+
+const env = process.env.NODE_ENV;
+if (env ==="development")
+{
+    export const API_BASE_URL = 'http://127.0.0.1:8000/';
+}
+else if (env ==="production"){
+export const API_BASE_URL = 'http://ec2-34-244-62-183.eu-west-1.compute.amazonaws.com/';
+}
