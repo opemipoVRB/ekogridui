5dae26f0535e6af8a06a070f64a30d0659d6f544
opemipoVRB <opemipodurodola@gmail.com>
2020-06-18 00:05:50 +0100

    Map Visualization

--- src/constants/apiContants.js
+++ src/constants/apiContants.js
@@ -2,7 +2,7 @@
 const env = process.env.NODE_ENV;
 export const API_BASE_URL = (env ==="development") ? 'http://127.0.0.1:8000/'  : 'http://ec2-34-244-62-183.eu-west-1.compute.amazonaws.com/';
 export const WS_BASE_URL = (env ==="development") ? 'ws://127.0.0.1:8000/'  : 'ws://ec2-34-244-62-183.eu-west-1.compute.amazonaws.com/';
-export const MapboxAccessToken=process.env.MapboxAccessToken;
+export const MapboxAccessToken=process.env.REACT_MapboxAccessToken;
 console.log(API_BASE_URL);
 
 
